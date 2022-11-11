7ad972338490c45bb4dae35d912842571430558f
HelloWorld152 <15224718167@163.com>
2022-11-11 17:32:19 +0800

    更新

--- rabbitmqTest.py
+++ rabbitmqTest.py
@@ -1,3 +1,12 @@
 import pika
+from loguru import logger
 
-connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+# connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+logger.add('web.log')
+logger.debug('hello')
+logger.info('world')
+logger.warning('1')
+logger.error('2')
+logger.info('3')
+logger.success('4')
+logger.critical('5')
